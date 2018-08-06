3d01b1cad5374db065f3abcf0d50bd2437ba3225
Slasten Tikhon <slasten32@gmail.com>
2018-08-06 11:38:33 +1000

    Fix kitchen items query

--- app/Http/Controllers/KitchenController.php
+++ app/Http/Controllers/KitchenController.php
@@ -18,18 +18,18 @@ class KitchenController extends Controller
         if (Auth::check() && Auth::user()->can('view_kitchen', $kitchen))
         {
             $grouped = $request->input('grouped', false) === "true";
-            $rows = OrderList::join('order_list_items', 'order_list_items.order_list_id', '=', 'order_lists.id')
+            $items = OrderList::join('order_list_items', 'order_list_items.order_list_id', '=', 'order_lists.id')
                 ->join('suppliers', 'suppliers.id', '=', 'order_list_items.supplier_id')
                 ->join('items', 'items.id', '=', 'order_list_items.item_id')
                 ->join('item_categories', 'item_categories.id', '=', 'items.category_id')
-                ->where('order_list_items.kitchen_id', '=', $kitchen->id)
+                ->where('order_lists.kitchen_id', '=', $kitchen->id)
                 ->orderBy('order_lists.note')
                 ->select('*',
                     DB::raw('(select count(*) from order_list_items t where t.order_list_id = `order_lists`.id) as count'));
-            $rows = ($grouped ? $rows->orderBy('item_categories.name', 'asc') : $rows);
-            $rows = $rows->orderBy('order_list_items.kitchen_sort_order', 'asc')->get();
+            $items = ($grouped ? $items->orderBy('item_categories.name', 'asc') : $items);
+            $items = $items->orderBy('order_list_items.kitchen_sort_order', 'asc')->get();
             return view('pages.kitchen', ['grouped' => $grouped,
-                'kitchen' => $kitchen, 'rows' => $rows, 'suppliers' => Supplier::all(),
+                'kitchen' => $kitchen, 'items' => $items, 'suppliers' => Supplier::all(),
                 'all_items' => Item::all()]);
         }
         return redirect()->back();
