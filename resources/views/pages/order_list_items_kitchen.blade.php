067b4619d89b8c300981af41ea381255bb6ff517
Slasten Tikhon <slasten32@gmail.com>
2018-08-24 20:46:06 +1000

    Fix margin between order list items

--- resources/views/pages/order_list_items_kitchen.blade.php
+++ resources/views/pages/order_list_items_kitchen.blade.php
@@ -2,13 +2,13 @@
 
 @section('order_list_items')
     @php($readonly = $order_list->completed && !Auth::user()->is_manager ? 'readonly' : '')
-    @foreach($order_list_items as $item)
-        <div class="list-group-item">
+    <div class="list-group-item">
+        @foreach($order_list_items as $item)
             @include('partials.order_list_item_form_update', ['item' => $item, 'suppliers' => $suppliers, 'errors' => $errors, 'readonly' => $readonly])
             <form id="delete_{{ $item->id }}" action="{{ route('order_list_item.delete') }}" method="POST" style="display: none;">
                 @csrf
                 <input type="hidden" name="id" value="{{ $item->id }}">
             </form>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 @endsection
