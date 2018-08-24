24de0d1db5fb90b1a59263a183ccc595fc24256d
Slasten Tikhon <slasten32@gmail.com>
2018-08-24 21:27:43 +1000

    Add pagebreak after each supplier in PDF

--- resources/views/pdf/suppliers.blade.php
+++ resources/views/pdf/suppliers.blade.php
@@ -5,37 +5,43 @@
     <title>{{ $order_list_name }} | Order list</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style>
+        .page-break {
+            page-break-before: always;
+        }
+    </style>
 </head>
 <body>
 @php($i = 0)
 @while($i < count($order_list_items))
-    <h3>{{ $order_list_items[$i]->supplier->name }}</h3>
-    <table style="width: 100%;" class="table table-sm table-striped">
-        <thead>
-        <tr>
-            <th scope="col">{{ __('supplier_pdf.num') }}</th>
-            <th scope="col">{{ __('supplier_pdf.full_name') }}</th>
-            <th scope="col">{{ __('supplier_pdf.quantity') }}</th>
-            <th scope="col">{{ __('supplier_pdf.cost') }}</th>
-        </tr>
-        </thead>
-        <tbody>
-            @php($current_supplier = $order_list_items[$i]->supplier_id)
-            @php($row_id = 1)
-            @while($i < count($order_list_items) && $order_list_items[$i]->supplier_id == $current_supplier)
-                @php($order_list_item = $order_list_items[$i])
-                <tr>
-                    <th scope="row">{{ $row_id }}</th>
-                    <td>{{ $order_list_item->item->full_name }}</td>
-                    <td>{{ $order_list_item->quantity }}</td>
-                    <td>{{ $order_list_item->item->cost }}</td>
-                </tr>
-                @php($i++)
-                @php($row_id++)
-            @endwhile
-        </tbody>
-    </table>
-    <br>
+    <div class="{{ $i == 0 ? '' : 'page-break' }}">
+        <h3>{{ $order_list_items[$i]->supplier->name }}</h3>
+        <table style="width: 100%;" class="table table-sm table-striped">
+            <thead>
+            <tr>
+                <th scope="col">{{ __('supplier_pdf.num') }}</th>
+                <th scope="col">{{ __('supplier_pdf.full_name') }}</th>
+                <th scope="col">{{ __('supplier_pdf.quantity') }}</th>
+                <th scope="col">{{ __('supplier_pdf.cost') }}</th>
+            </tr>
+            </thead>
+            <tbody>
+                @php($current_supplier = $order_list_items[$i]->supplier_id)
+                @php($row_id = 1)
+                @while($i < count($order_list_items) && $order_list_items[$i]->supplier_id == $current_supplier)
+                    @php($order_list_item = $order_list_items[$i])
+                    <tr>
+                        <th scope="row">{{ $row_id }}</th>
+                        <td>{{ $order_list_item->item->full_name }}</td>
+                        <td>{{ $order_list_item->quantity }}</td>
+                        <td>{{ $order_list_item->item->cost }}</td>
+                    </tr>
+                    @php($i++)
+                    @php($row_id++)
+                @endwhile
+            </tbody>
+        </table>
+    </div>
 @endwhile
 </body>
 </html>
