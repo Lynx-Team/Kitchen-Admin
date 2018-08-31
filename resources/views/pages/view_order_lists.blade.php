00c46d9f0cb96afbcaf9afdd62b67c19d458d68e
Slasten Tikhon <slasten32@gmail.com>
2018-08-31 21:42:41 +1000

    Fix order lists view

--- resources/views/pages/view_order_lists.blade.php
+++ resources/views/pages/view_order_lists.blade.php
@@ -38,45 +38,52 @@
     @endif
     @forelse($order_lists as $order_list)
         <div class="row justify-content-center mb-2">
-            <div class="card col-12">
-                <div class="card-body row">
-                    <div class="col-9">
-                        <h5>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body row">
+                        <div class="col-9">
+                            <h5>
+                                @if(Auth::user()->can('update', $order_list))
+                                    <form id="update_{{ $order_list->id }}" method="POST" action="{{ route('order_lists.update') }}" class="row">
+                                        @csrf
+                                        <input type="hidden" name="id" value="{{ $order_list->id }}">
+                                        <input type="hidden" name="kitchen_id" value="{{ $order_list->kitchen_id }}">
+                                        <div class="form-group col-5">
+                                            <input type="text" name="note" class="form-control" value="{{ $order_list->note }}">
+                                        </div>
+                                        <div class="col-2">
+                                            <span class="badge badge-primary badge-pill">Items: {{ $order_list->order_list_items_count}}</span>
+                                        </div>
+                                    </form>
+                                @else
+                                {{ $order_list->note }}
+                                    <span class="badge badge-primary badge-pill">Items: {{ $order_list->order_list_items_count}}</span>
+                                @endif
+                            </h5>
+                        </div>
+                        <form id="delete_{{ $order_list->id }}" method="POST" action="{{ route('order_lists.delete') }}" style="display: none;">
+                            @csrf
+                            <input type="hidden" name="id" value="{{ $order_list->id }}">
+                        </form>
+                        <div class="col-3">
+                            <a href="{{ route('order_list_items.view', ['kitchen_id' => Request::segment(2), 'order_list_id' => $order_list->id]) }}" class="btn btn-success">
+                                View
+                            </a>
                             @if(Auth::user()->can('update', $order_list))
-                                <form id="update_{{ $order_list->id }}" method="POST" action="{{ route('order_lists.update') }}">
-                                    @csrf
-                                    <input type="hidden" name="id" value="{{ $order_list->id }}">
-                                    <input type="text" name="note" value="{{ $order_list->note }}">
-                                    <input type="hidden" name="kitchen_id" value="{{ $order_list->kitchen_id }}">
-                                </form>
-                            @else
-                            {{ $order_list->note }}
+                                <a class="btn btn-warning"
+                                   onclick="event.preventDefault();document.getElementById('update_{{ $order_list->id }}').submit();">
+                                    Update
+                                </a>
                             @endif
-                            <span class="badge badge-primary badge-pill">Items: {{ $order_list->order_list_items_count}}</span>
-                        </h5>
-                    </div>
-                    <form id="delete_{{ $order_list->id }}" method="POST" action="{{ route('order_lists.delete') }}" style="display: none;">
-                        @csrf
-                        <input type="hidden" name="id" value="{{ $order_list->id }}">
-                    </form>
-                    <div class="col-3">
-                        <a href="{{ route('order_list_items.view', ['kitchen_id' => Request::segment(2), 'order_list_id' => $order_list->id]) }}" class="btn btn-success">
-                            View
-                        </a>
-                        @if(Auth::user()->can('update', $order_list))
-                            <a class="btn btn-warning"
-                               onclick="event.preventDefault();document.getElementById('update_{{ $order_list->id }}').submit();">
-                                Update
-                            </a>
-                        @endif
-                        @if(Auth::user()->can('delete', $order_list))
-                            <a class="btn btn-danger"
-                               data-toggle="modal" data-target="#confirmDelete"
-                               data-title="Delete order list" data-message="Are you sure you want to delete this order list?"
-                               data-form-id="delete_{{ $order_list->id }}">
-                                Delete
-                            </a>
-                        @endif
+                            @if(Auth::user()->can('delete', $order_list))
+                                <a class="btn btn-danger"
+                                   data-toggle="modal" data-target="#confirmDelete"
+                                   data-title="Delete order list" data-message="Are you sure you want to delete this order list?"
+                                   data-form-id="delete_{{ $order_list->id }}">
+                                    Delete
+                                </a>
+                            @endif
+                        </div>
                     </div>
                 </div>
             </div>
