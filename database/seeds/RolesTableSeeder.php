6915bad9ce58b4dbbc6cb1decd7288c58e9d120e
KeltarHelviett <keltarhelviett@gmail.com>
2018-08-31 22:13:00 +1000

    Add new roles to RolesTableSeeder

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolesTableSeeder extends Seeder
 {
@@ -14,12 +15,16 @@ class RolesTableSeeder extends Seeder
         $this->insert_role('admin');
         $this->insert_role('kitchen');
         $this->insert_role('manager');
+        $this->insert_role('superuser');
+        $this->insert_role('customer');
     }
 
     private function insert_role(string $role_name)
     {
-        DB::table('roles')->insert([
-            'name' => $role_name
-        ]);
+        if (!DB::table('roles')->where('name', $role_name)->exists()){
+            DB::table('roles')->insert([
+                'name' => $role_name
+            ]);
+        }
     }
 }
