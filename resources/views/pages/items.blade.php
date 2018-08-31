4e50a1f6d0acba2ab8f8070c3b7eba374ac5a2a4
Slasten Tikhon <slasten32@gmail.com>
2018-09-01 02:02:04 +1000

    Improve products list view

--- resources/views/pages/items.blade.php
+++ resources/views/pages/items.blade.php
@@ -106,107 +106,128 @@
         </div>
     </div>
     <div class="row justify-content-center">
-        @forelse ($items as $item)
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">{{$item->short_name }}</div>
-                    <div class="card-body">
-                        <form action="{{ route('items.update') }}" method="POST">
-                            @csrf
-                            <input type="hidden" name="id" value="{{ $item->id }}">
-                            <div class="form-group row">
-                                <input type="text" class="form-control" name="short_name" value="{{ $item->short_name }}" placeholder="{{ __('items.short_name') }}" aria-describedby="update_short_name_error">
-                                @if($errors->update->has('short_name') && $errors->update->first('row_id') == $item->id)
-                                    <p id="update_short_name_error" class="form-text text-danger">
-                                        {{ $errors->update->first('short_name') }}
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="form-group row">
-                                <input type="text" class="form-control" name="full_name" value="{{ $item->full_name }}" placeholder="{{ __('items.full_name') }}" aria-describedby="update_full_name_error">
-                                @if($errors->update->has('full_name') && $errors->update->first('row_id') == $item->id)
-                                    <p id="update_full_name_error" class="form-text text-danger">
-                                        {{ $errors->update->first('full_name') }}
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="form-group row">
-                                <input type="text" class="form-control" name="unit" value="{{ $item->unit}}"
-                                       placeholder="{{ __('items.unit') }}" aria-describedby="update_unit_error">
-                                @if($errors->update->has('unit') && $errors->update->first('row_id') == $item->id)
-                                    <p id="update_unit_error" class="form-text text-danger">
-                                        {{ $errors->update->first('unit') }}
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="form-group row">
-                                <select class="form-control" name="default_supplier" aria-describedby="update_default_supplier_error">
-                                    @foreach($suppliers as $supplier)
-                                        <option value="{{ $supplier->id }}" {{ $item->default_supplier_id == $supplier->id ? 'selected' : '' }}>
-                                            {{ $supplier->name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @if($errors->update->has('default_supplier') && $errors->update->first('row_id') == $item->id)
-                                    <p id="update_default_supplier_error" class="form-text text-danger">
-                                        {{ $errors->update->first('default_supplier') }}
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="form-group row">
-                                <select class="form-control" name="category" aria-describedby="update_category_error">
-                                    @foreach($categories as $category)
-                                        <option value="{{ $category->id }}" {{ $item->category_id == $category->id ? 'selected' : '' }}>
-                                            {{ $category->name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @if($errors->update->has('category') && $errors->update->first('row_id') == $item->id)
-                                    <p id="update_category_error" class="form-text text-danger">
-                                        {{ $errors->update->first('category') }}
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="form-group row">
-                                <input type="number" min="0" step="0.01" class="form-control" name="cost" value="{{ $item->cost }}" placeholder="{{ __('items.cost') }}" aria-describedby="update_cost_error">
-                                @if($errors->update->has('cost') && $errors->update->first('row_id') == $item->id)
-                                    <p id="update_cost_error" class="form-text text-danger">
-                                        {{ $errors->update->first('cost') }}
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="form-group row">
-                                <input type="text" class="form-control" name="product_code" value="{{ $item->product_code }}"
-                                       placeholder="{{ __('items.product_code') }}" aria-describedby="update_product_code_error">
-                                @if($errors->update->has('product_code') && $errors->update->first('row_id') == $item->id)
-                                    <p id="update_product_code_error" class="form-text text-danger">
-                                        {{ $errors->update->first('product_code') }}
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="form-group row">
-                                <button type="submit" class="btn btn-warning mr-3">{{ __('items.update_btn') }}</button>
-                                <a role="button" class="btn btn-danger" data-toggle="modal" data-target="#confirmDelete"
+        <div class="col-md-12">
+            <div class="accordion" id="items-list">
+                @forelse ($items as $item)
+                    <div class="card" style="margin-bottom: 0;">
+                        <div class="card-header" id="item-heading-{{ $item->id }}">
+                            <div class="row justify-content-between">
+                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#item-{{ $item->id }}" aria-expanded="true" aria-controls="item-{{ $item->id }}">
+                                    {{$item->short_name }}
+                                </button>
+                                <a role="button" class="btn btn-danger mr-3" data-toggle="modal" data-target="#confirmDelete"
                                    data-title="Delete item" data-message="Are you sure you want to delete this item?"
                                    data-form-id="delete_{{ $item->id }}">
                                     {!! __('items.delete_btn') !!}
                                 </a>
                             </div>
-                        </form>
-                        <form id="delete_{{ $item->id }}" action="{{ route('items.delete') }}" method="POST" style="display: none;">
-                            @csrf
-                            <input type="hidden" name="id" value="{{ $item->id }}">
-                        </form>
+                        </div>
+                        <div id="item-{{ $item->id }}" class="collapse" aria-labelledby="item-heading-{{ $item->id }}" data-parent="#items-list">
+                            <div class="card-body">
+                                <form action="{{ route('items.update') }}" method="POST">
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{ $item->id }}">
+                                    <div class="row">
+                                        <div class="form-group col">
+                                            <label>{{ __('items.short_name') }}</label>
+                                            <input type="text" class="form-control" name="short_name" value="{{ $item->short_name }}" placeholder="{{ __('items.short_name') }}" aria-describedby="update_short_name_error">
+                                            @if($errors->update->has('short_name') && $errors->update->first('row_id') == $item->id)
+                                                <p id="update_short_name_error" class="form-text text-danger">
+                                                    {{ $errors->update->first('short_name') }}
+                                                </p>
+                                            @endif
+                                        </div>
+                                        <div class="form-group col">
+                                            <label>{{ __('items.full_name') }}</label>
+                                            <input type="text" class="form-control" name="full_name" value="{{ $item->full_name }}" placeholder="{{ __('items.full_name') }}" aria-describedby="update_full_name_error">
+                                            @if($errors->update->has('full_name') && $errors->update->first('row_id') == $item->id)
+                                                <p id="update_full_name_error" class="form-text text-danger">
+                                                    {{ $errors->update->first('full_name') }}
+                                                </p>
+                                            @endif
+                                        </div>
+                                        <div class="form-group col">
+                                            <label>{{ __('items.unit') }}</label>
+                                            <input type="text" class="form-control" name="unit" value="{{ $item->unit}}"
+                                                   placeholder="{{ __('items.unit') }}" aria-describedby="update_unit_error">
+                                            @if($errors->update->has('unit') && $errors->update->first('row_id') == $item->id)
+                                                <p id="update_unit_error" class="form-text text-danger">
+                                                    {{ $errors->update->first('unit') }}
+                                                </p>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col">
+                                            <label>{{ __('items.default_supplier') }}</label>
+                                            <select class="form-control" name="default_supplier" aria-describedby="update_default_supplier_error">
+                                                @foreach($suppliers as $supplier)
+                                                    <option value="{{ $supplier->id }}" {{ $item->default_supplier_id == $supplier->id ? 'selected' : '' }}>
+                                                        {{ $supplier->name }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                            @if($errors->update->has('default_supplier') && $errors->update->first('row_id') == $item->id)
+                                                <p id="update_default_supplier_error" class="form-text text-danger">
+                                                    {{ $errors->update->first('default_supplier') }}
+                                                </p>
+                                            @endif
+                                        </div>
+                                        <div class="form-group col">
+                                            <label>{{ __('items.category') }}</label>
+                                            <select class="form-control" name="category" aria-describedby="update_category_error">
+                                                @foreach($categories as $category)
+                                                    <option value="{{ $category->id }}" {{ $item->category_id == $category->id ? 'selected' : '' }}>
+                                                        {{ $category->name }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                            @if($errors->update->has('category') && $errors->update->first('row_id') == $item->id)
+                                                <p id="update_category_error" class="form-text text-danger">
+                                                    {{ $errors->update->first('category') }}
+                                                </p>
+                                            @endif
+                                        </div>
+                                        <div class="form-group col">
+                                            <label>{{ __('items.cost') }}</label>
+                                            <input type="number" min="0" step="0.01" class="form-control" name="cost" value="{{ $item->cost }}" placeholder="{{ __('items.cost') }}" aria-describedby="update_cost_error">
+                                            @if($errors->update->has('cost') && $errors->update->first('row_id') == $item->id)
+                                                <p id="update_cost_error" class="form-text text-danger">
+                                                    {{ $errors->update->first('cost') }}
+                                                </p>
+                                            @endif
+                                        </div>
+                                        <div class="form-group col">
+                                            <label>{{ __('items.product_code') }}</label>
+                                            <input type="text" class="form-control" name="product_code" value="{{ $item->product_code }}"
+                                                   placeholder="{{ __('items.product_code') }}" aria-describedby="update_product_code_error">
+                                            @if($errors->update->has('product_code') && $errors->update->first('row_id') == $item->id)
+                                                <p id="update_product_code_error" class="form-text text-danger">
+                                                    {{ $errors->update->first('product_code') }}
+                                                </p>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="form-group row justify-content-end">
+                                        <button type="submit" class="btn btn-warning mr-3">{{ __('items.update_btn') }}</button>
+                                    </div>
+                                </form>
+                                <form id="delete_{{ $item->id }}" action="{{ route('items.delete') }}" method="POST" style="display: none;">
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{ $item->id }}">
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                @empty
+                    <div class="col">
+                        <div class="alert alert-warning" role="alert">
+                            {{ __('items.no_items') }}
+                        </div>
+                    </div>
+                @endforelse
             </div>
-        @empty
-            <div class="col">
-                <div class="alert alert-warning" role="alert">
-                    {{ __('items.no_items') }}
-                </div>
-            </div>
-        @endforelse
+        </div>
     </div>
 @endsection
 
