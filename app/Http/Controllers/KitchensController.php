75a27575e35e3f6a637a97826eb13300ee45b343
KeltarHelviett <keltarhelviett@gmail.com>
2018-09-02 17:21:56 +1000

    See only available kitchens

--- app/Http/Controllers/KitchensController.php
+++ app/Http/Controllers/KitchensController.php
@@ -16,12 +16,13 @@ class KitchensController extends Controller
     {
         if (Auth::check() && Auth::user()->can('view_kitchens', User::class))
         {
+            $cutomerId = Auth::user()->customer()->id;
             $kitchens = DB::select("
               select users.id, users.name, count(order_lists.id) as order_lists_number
               from users
               join roles on roles.id = users.role_id 
               left join order_lists on order_lists.kitchen_id = users.id
-              join customer_workers  
+              join customer_workers cw on (cw.worker_id = users.id and cw.customer_id = {$cutomerId})
               where roles.name = 'kitchen' 
               group by users.id, users.name");
             return view('pages.kitchens', ['kitchens' => $kitchens]);
